c59f2671fc2ca0cadee8d29c1b1eabb283ae995f
Aitana Rus <ruslopezaitana@gmail.com>
2022-10-04 14:16:42 +0200

    intro and main activity layout

--- app/src/main/java/com/example/pharmafast/MainActivity.java
+++ app/src/main/java/com/example/pharmafast/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pharmafast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -10,10 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
     }
-
-
-
-
-
 }
